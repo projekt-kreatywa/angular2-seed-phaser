72610f16c5b00f33382be00f3d03a5c67c7623de
Tiago Marques <tiago.marques@yieldify.com>
2016-07-24 14:10:55 +0100

    added phaser2 package, typings and libs

--- karma.conf.js
+++ karma.conf.js
@@ -31,6 +31,9 @@ module.exports = function(config) {
       'node_modules/zone.js/dist/async-test.js',
       'node_modules/zone.js/dist/fake-async-test.js',
 
+      // Phaser dependencies
+      'node_modules/phaser/build/phaser.js',
+
       // RxJs.
       { pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false },
       { pattern: 'node_modules/rxjs/**/*.js.map', included: false, watched: false },
